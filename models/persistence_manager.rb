50826c639eb489a3e0db1aca8dfa23645eb9e038
egvghost <egvghost@gmail.com>
2018-11-21 19:15:19 -0300

    minor wording fixes

--- models/persistence_manager.rb
+++ models/persistence_manager.rb
@@ -4,25 +4,25 @@ class PersistenceManager
   def initialize
     @archivo_de_obras = YAML::Store.new 'obras.yml'
     @archivo_de_obras.transaction do
-      @archivo_de_obras['lista_de_obras'] = [] if @archivo_de_obras['lista_de_obras'].nil?
+      @archivo_de_obras['lista_obras'] = [] if @archivo_de_obras['lista_obras'].nil?
     end
   end
 
-  def agregar_obra(obra)
-    raise ObrasException.new 'Ya existe una obra con ese ID' if lista_de_obras.include?(obra)
+  def crear_obra(obra)
+    raise ObrasException.new 'Ya existe una obra con ese ID' if lista_obras.include?(obra)
     @archivo_de_obras.transaction do
-      @archivo_de_obras['lista_de_obras'] << obra
+      @archivo_de_obras['lista_obras'] << obra
     end
   end
 
-  def listar_obras
+  def lista_obras
     @archivo_de_obras.transaction do
-      @archivo_de_obras['lista_de_obras']
+      @archivo_de_obras['lista_obras']
     end
   end
 
   def encontrar_obra(id_obra)
-    obra_elegida = lista_de_obras.select{ |obra| obra.id == id_obra}
+    obra_elegida = lista_obras.select{ |obra| obra.id == id_obra}
     obra_elegida.first
   end
 
@@ -30,7 +30,7 @@ class PersistenceManager
     obra = encontrar_obra id_obra
     raise ObraException.new 'Obra no encontrada' if obra.nil?
     @archivo_de_obras.transaction do
-      @archivo_de_obras['lista_de_obras'].delete_if {|obra| obra.id == id_obra}
+      @archivo_de_obras['lista_obras'].delete_if {|obra| obra.id == id_obra}
     end
   end
 
@@ -38,8 +38,8 @@ class PersistenceManager
     obra_previa = encontrar_obra(obra_nueva.id)
     raise ObraException.new 'Obra no encontrada' if obra_previa.nil?
     @archivo_de_obras.transaction do
-      @archivo_de_obras['lista_de_obras'].delete(obra_previa)
-      @archivo_de_obras['lista_de_obras'] << obra_nueva
+      @archivo_de_obras['lista_obras'].delete(obra_previa)
+      @archivo_de_obras['lista_obras'] << obra_nueva
     end
   end
 
