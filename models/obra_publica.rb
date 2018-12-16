dbe17d4d418e6fbadb14bb69030b0234716ace88
egvghost <egvghost@gmail.com>
2018-12-16 00:41:47 -0300

    exception added for invalid date format

--- models/obra_publica.rb
+++ models/obra_publica.rb
@@ -1,4 +1,5 @@
 require 'byebug'
+require 'date'
 require './models/obras_parser.rb'
 
 class ObraPublica
@@ -23,16 +24,22 @@ class ObraPublica
     @fecha_inicio = fecha_inicio.to_s
     @fecha_fin_planeada = fecha_fin_planeada.to_s
     @fecha_fin_real = fecha_fin_real.to_s
+    #byebug
+    begin
+      @fecha_inicio = Date.parse(@fecha_inicio).to_s unless @fecha_inicio.empty?
+      @fecha_fin_planeada = Date.parse(@fecha_fin_planeada).to_s unless @fecha_fin_planeada.empty?
+      @fecha_fin_real = Date.parse(@fecha_fin_real).to_s unless @fecha_fin_real.empty?
+    rescue => exception
+      raise InputException.new("ID:#{@id} - El formato de fecha ingresado no es válido")
+    end
     @porcentaje_avance = porcentaje_avance.to_i
     @imagen = imagen
     etapas = ['En Ejecución', 'En Licitación', 'En Proyecto', 'Finalizada']
-    #byebug
     raise InputException.new("ID:#{@id} - La etapa ingresada no coincide con las opciones permitidas") unless etapas.include?(@etapa)
     raise InputException.new("ID:#{@id} - El porcentaje de avance ingresado no está permitido") unless (@porcentaje_avance >= 0 && @porcentaje_avance <= 100)
     raise InputException.new("ID:#{@id} - El monto ingresado no puede ser negativo") unless (@monto_contrato >= 0)
     raise InputException.new("ID:#{@id} - El número de comuna debe estar entre 1 y 15") unless (@comuna >= 1 && @comuna <=15)
     raise InputException.new("ID:#{@id} - La descripción no puede contener más de 2000 caracteres") unless (@descripcion.length <= 2000)
-    #raise InputException.new('El formato de fecha ingresado no es válido') unless...
     unless (@fecha_inicio.empty? || @fecha_fin_planeada.empty?)
       raise InputException.new("ID:#{@id} - La fecha final estimada debe ser posterior a la fecha de inicio") unless @fecha_inicio < @fecha_fin_planeada
     end
